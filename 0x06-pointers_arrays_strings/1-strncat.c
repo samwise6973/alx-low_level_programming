2e5b51bd2299d474e8ace88afafc076436c05588
Oluwashina <oluwashinabalogun2@gmail.com>
2022-06-16 13:19:19 -0700

    Strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
- * _strncpy - Copies at most an inputted number
- * of bytes from string src into dest.
- * @dest: The buffer storing the string copy.
- * @src: The source string.
- * @n: The maximum number of bytes to copied from src.
+ * _strncat - Concatenates two strings using at most
+ * 	 an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
  *
  * Return: A pointer to the resulting string dest.
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+	int index = 0, dest_len = 0;
 
-	while (src[index++])
-		src_len++;
+	while (dest[index++])
+		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
