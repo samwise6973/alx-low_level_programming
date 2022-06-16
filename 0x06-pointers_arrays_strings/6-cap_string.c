69e8cabbb03358b367350b91255c1cee56ba57bd
Oluwashina <oluwashinabalogun2@gmail.com>
2022-06-16 14:02:59 -0700

    Cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,6 +3,7 @@
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
+ *
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
@@ -15,20 +16,20 @@ char *cap_string(char *str)
 			index++;
 
 		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}' ||
-				index == 0)
-				str[index] -= 32;
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+			str[index] -= 32;
 
 		index++;
 	}
